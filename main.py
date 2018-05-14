e03d59d2e8ff8a552fda42adbfa8ab896a81cb27
infpowertower <robertrichter_london@yahoo.de>
2018-05-14 23:17:45 +0200

    First testing

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 import sqlite3
+from pathlib import Path
+import os
 
-print("Hello world!")
+def create_db(db_name):
+    db_name = db_name+".db"
+    db_file = Path(db_name)
+    print(db_file)
+    if (db_file.is_file()):
+        print("ERROR - Database exists!")
+    else:
+        with sqlite3.connect(db_name) as db:
+            pass
 
-with sqlite3.connect('finanzreport.db') as db:
-    pass
+def delete_db(db_name):
+    db_name = db_name+".db"
+    db_file = Path(db_name)
+    print(db_file)
+    if (db_file.is_file()):
+        os.remove(db_name)
+    else:
+        print("ERROR - Database doesn't exist!")
+
+
+if (__name__ == "__main__"):
+    delete_db("settings")
