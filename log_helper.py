af9f0240b16f39c42358ca69943b8c059792c587
robert <robertrichter_london@yahoo.de>
2018-05-15 14:23:37 +0100

    moved logging and database out of main overhauled logging new directories for logging and databases

---
+++ log_helper.py
@@ -0,0 +1,40 @@
+import logging
+import os
+import shutil
+from pathlib import Path
+
+def init_log(name):
+    if not os.path.exists('log'):
+        os.makedirs('log')
+    FORMAT = '%(asctime)-15s %(message)s'
+    formatter = logging.Formatter(FORMAT)
+    logging.basicConfig(format=FORMAT)
+    logger = logging.getLogger(name)
+    file_path = 'log/' + name + '.log'
+    fh = logging.FileHandler(file_path)
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    # Stream Handler doesn't seem to be doing much.
+    """sh = logging.StreamHandler()
+    sh.setLevel(logging.WARNING)
+    sh.setFormatter(formatter)
+    logger.addHandler(sh)"""
+    return logger
+
+def delete_log(name):
+    path = 'log/' + name + ".log"
+    file = Path(path)
+    if (file.is_file()):
+        os.remove(path)
+        logging.getLogger('logging').info("delete_log: %s deleted", name)
+    else:
+        logging.getLogger('logging').info("delete_log: %s doesn't exist", path)
+
+def purge_logs():
+    in1 = input("Are you sure to delete ALL logs? [y/N]")
+    if (in1 == 'y' or in1 == 'Y'):
+        if os.path.exists('log'):
+            shutil.rmtree('log')
+            print("All logs were deleted. I hope you know what you're doing.")
--- main.py
+++ main.py
@@ -1,26 +1,17 @@
-import sqlite3
-from pathlib import Path
-import os
+import log_helper as log
+import database_helper as db
 
-def create_db(db_name):
-    db_name = db_name+".db"
-    db_file = Path(db_name)
-    print(db_file)
-    if (db_file.is_file()):
-        print("ERROR - Database exists!")
-    else:
-        with sqlite3.connect(db_name) as db:
-            pass
-
-def delete_db(db_name):
-    db_name = db_name+".db"
-    db_file = Path(db_name)
-    print(db_file)
-    if (db_file.is_file()):
-        os.remove(db_name)
-    else:
-        print("ERROR - Database doesn't exist!")
-
+def init_logs():
+    log.init_log('main')
+    log.init_log('database')
+    log.init_log('logging')
 
 if (__name__ == "__main__"):
-    delete_db("settings")
+    init_logs()
+    db.create_db('users')
+
+    #test
+    '''logging.getLogger('main').info('info')
+    logging.getLogger('main').warning("Warning!")
+    logging.getLogger('main').error("Whats happening??")
+    logging.getLogger('main').critical("I'm dying!")'''
